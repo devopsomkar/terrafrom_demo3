be900fab9229248dc30647aa4ae798bc6701f767
wani <owani@deloitte.com>
2026-04-14 15:32:00 +0530

    Target group name_prefix for uniqueness during replacement

--- modules/ecs-fargate/main.tf
+++ modules/ecs-fargate/main.tf
@@ -22,11 +22,11 @@ resource "aws_alb" "nginx_alb" {
 }
 
 resource "aws_lb_target_group" "nginx_tg" {
-  name        = "${var.ecs_cluster_name}-tg"
-  port        = var.container_port
-  protocol    = "HTTP"
-  vpc_id      = var.vpc_id
-  target_type = "ip"
+  name_prefix   = "${var.ecs_cluster_name}-tg-"
+  port          = var.container_port
+  protocol      = "HTTP"
+  vpc_id        = var.vpc_id
+  target_type   = "ip"
 
   health_check {
     enabled             = true
