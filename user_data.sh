5c7e61692d5434a0936d77854998e246a6e6ab8f
omkar subhash wani <vaniomkar5@gmaIl.com>
2026-04-14 14:27:11 +0530

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /tmp && \
+cd /tmp && \ 
 curl -L -H "Authorization: Api-Token dt0c01.JQCQRMPU27SN3NP35HZXZPVB.HIE7JD2D5MLYRIWMQ6WAF4PMJPQXYDTXCEFCWNMAB6HGZLNJM4Y6FXLRBC5VMJJA" \
 "https://azs89024.live.dynatrace.com/api/v1/deployment/installer/agent/unix/default/latest?arch=x86" \
 -o Dynatrace-OneAgent-Linux.sh
