05d5de221ee128bf028e29f877809efbb5214663
omkar subhash wani <vaniomkar5@gmaIl.com>
2026-04-15 09:59:17 +0530

    Update user_data.sh

--- modules/ec2-instance/user_data.sh
+++ modules/ec2-instance/user_data.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
 yum update -y
 yum install -y nginx
-systemctl start nginx
 systemctl enable nginx
+systemctl start nginx
 
-# Dynatrace OneAgent install
-TENANT=$(curl -s "http://169.254.169.254/latest/meta-data/instance-id")
-TOKEN="${dynatrace_token}"
-curl -H "Authorization: Api-Token ${TOKEN}" -H 'Content-Type: application/json' -X PUT "https://${dynatrace_tenant}/api/v1/host/${TENANT}?keepAlive=true" -d '{"autoUpdateEnabled":true}'
+# Create simple index.html
+cat > /usr/share/nginx/html/index.html << EOF
+<!DOCTYPE html>
+<html>
+<head><title>Nginx on EC2 with Dynatrace</title></head>
+<body>
+<h1>Hello from Nginx on Terraform EC2!</h1>
+<p>Monitored by Dynatrace.</p>
+</body>
+</html>
+EOF
 
+
+cd /tmp && \
+curl -L -H "Authorization: Api-Token dt0c01.JQCQRMPU27SN3NP35HZXZPVB.HIE7JD2D5MLYRIWMQ6WAF4PMJPQXYDTXCEFCWNMAB6HGZLNJM4Y6FXLRBC5VMJJA" \
+"https://azs89024.live.dynatrace.com/api/v1/deployment/installer/agent/unix/default/latest?arch=x86" \
+-o Dynatrace-OneAgent-Linux.sh
+
+
+ls -l /tmp/Dynatrace-OneAgent-Linux.sh
+head -5 /tmp/Dynatrace-OneAgent-Linux.sh
+file /tmp/Dynatrace-OneAgent-Linux.sh
+
+/bin/sh /tmp/Dynatrace-OneAgent-Linux.sh \
+--set-monitoring-mode=fullstack \
+--set-app-log-content-access=true
